684f46ce8f67f7e3a6cdde149544b4878cb4cd3b
dynamic1717 <paska-andrej@yandex.ru>
2024-02-21 14:41:18 +0600

    Added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
-import node from '@astrojs/node'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import node from '@astrojs/node';
+import react from '@astrojs/react';
 
-import react from '@astrojs/react'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx(), react()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
-})
+  adapter: vercel()
+});
